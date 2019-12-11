c3edb069758445d8bbb833f2a73156ebaa72cab1
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2019-12-11 17:46:05 +0900

    test

---
+++ bocabook3/bocabook3/AddMemoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMemoViewController.swift
+//  bocabook3
+//
+//  Created by 高江洲　壱星 on 2019/12/11.
+//  Copyright © 2019 高江洲　壱星. All rights reserved.
+//
+
+import UIKit
+
+class AddMemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
